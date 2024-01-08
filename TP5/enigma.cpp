1ea57e9bb78a61954a4e95c5cdb911d78cc05ebf
Yohann Turpin <yt806741@ad.u-bourgogne.fr>
2024-01-08 11:52:39 +0100

    Ajout encode 2 rotor

--- TP5/enigma.cpp
+++ TP5/enigma.cpp
@@ -2,49 +2,45 @@
 #include <iostream>
 namespace enigma
 {
-    Enigma::Enigma(std::vector <int> keyValue = {'B', 'D', 'A', 'C', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}):_key(keyValue){}
+    Enigma::Enigma(std::vector <char> keyValue = {'B', 'D', 'A', 'C', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'},std::vector <char> keyValue2 ={'Z', 'D', 'T', 'C', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'A', 'U', 'V', 'W', 'X', 'Y', 'B'}):_key(keyValue),_key2(keyValue2){}
 
-    void Enigma::encode() {
-        std::string result = "";
+    
 
-        for (char c : getPlain()) {
-            if (std::isalpha(c)) {
-                char base = (std::isupper(c)) ? 'A' : 'a';
-                int index = std::toupper(c) - 'A';
-
-                c = _key[index];
-
-                // Rotation du rotor
-                std::rotate(_key.begin(), _key.begin() + 1, _key.end());
-            }
-            // else: caractères spéciaux non modifiés
-            result += c;
-        }
-
-        setCipher(result);
+     void Enigma::rotateRotor(std::vector<char>& rotor) {
+        std::rotate(rotor.begin(), rotor.begin() + 1, rotor.end());
     }
 
-    void Enigma::decode() {
-        std::string result = "";
+    void Enigma::encode() {
+    std::string result = "";
+    int rotor1Counter = 0;
 
-        for (char c : getCipher()) {
-            if (std::isalpha(c)) {
-                char base = (std::isupper(c)) ? 'A' : 'a';
-                int index = std::toupper(c) - 'A';
+    for (char c : getPlain()) {
+        if (std::isalpha(c)) {
+            char base = (std::isupper(c)) ? 'A' : 'a';
 
-                // Recherche de l'indice original dans le rotor
-                auto originalIndex = std::find(_key.begin(), _key.end(), c) - _key.begin();
+            // Passage à travers le premier rotor
+            int index1 = std::toupper(c) - 'A';
+            c = _key[index1];
 
-                c = base + originalIndex;
+            // Rotation du premier rotor
+            rotateRotor(_key);
+            rotor1Counter++;
 
-                // Rotation du rotor
-                std::rotate(_key.begin(), _key.begin() + 1, _key.end());
+            // Si 26 rotations complètes du premier rotor, tourner le deuxième rotor
+            if (rotor1Counter == 26) {
+                rotateRotor(_key2);
+                rotor1Counter = 0;  // Réinitialiser le compteur
             }
-            // else: caractères spéciaux non modifiés
-            result += c;
         }
+        // else: caractères spéciaux non modifiés
+        result += c;
+    }
 
-        setPlain(result);
+    setCipher(result);
+}
+
+    void Enigma::decode() {
+       
     }
 
 
